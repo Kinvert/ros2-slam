11fc445e88d4cf16be936ba3d5765502bc0b4536
Keith <keith@kinvert.com>
2025-06-26 06:08:38 +0000

    simple cam setup py

---
+++ src/simple_cam/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'simple_camera'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='you',
+    maintainer_email='you@example.com',
+    description='Simple camera publisher',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'camera_publisher = simple_camera.camera_publisher:main',
+        ],
+    },
+)
